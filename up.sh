1618441dfdb814433f1d1df516968f1df022a5a2
EC2 <temp@monitory.space>
2025-05-28 08:26:28 +0000

    fix: docker compose, script

--- up.sh
+++ up.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml up -d --build
+docker-compose -f docker-compose-service.yml down -v
+docker-compose -f docker-compose-grafana.yml down -v
+docker-compose -f docker-compose-source.yml down -v
+docker-compose -f docker-compose.yml down -v
+
+aws ecr get-login-password --region ap-northeast-2 \
+ | docker login --username AWS --password-stdin 853660505909.dkr.ecr.ap-northeast-2.amazonaws.com
+
+docker-compose -f docker-compose.yml up -d --pull always
 
 sleep 3
 
-docker-compose -f docker-compose-source.yml up -d --build
+docker-compose -f docker-compose-source.yml up -d --pull always
 
 sleep 3
 
-docker-compose -f docker-compose-grafana.yml up -d --build
+docker-compose -f docker-compose-grafana.yml up -d --pull always
 
 sleep 3
 
-docker-compose -f docker-compose-service.yml up -d --build
+docker-compose -f docker-compose-service.yml up -d --pull always
