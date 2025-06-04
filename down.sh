aed32c3ffebca7aabccb2780e07c34f7fa07ec4e
EC2 <temp@monitory.space>
2025-06-04 03:31:04 +0000

    feat: model server 추가

--- down.sh
+++ down.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+docker-compose -f docker-compose-model.yml down -v
 docker-compose -f docker-compose-service.yml down -v
 docker-compose -f docker-compose-grafana.yml down -v
 docker-compose -f docker-compose-source.yml down -v
 docker-compose -f docker-compose.yml down -v
-
+docker image prune -f
--- up.sh
+++ up.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+docker-compose -f docker-compose-model.yml down -v
 docker-compose -f docker-compose-service.yml down -v
 docker-compose -f docker-compose-grafana.yml down -v
 docker-compose -f docker-compose-source.yml down -v
 docker-compose -f docker-compose.yml down -v
+docker image prune -f
 
 aws ecr get-login-password --region ap-northeast-2 \
  | docker login --username AWS --password-stdin 853660505909.dkr.ecr.ap-northeast-2.amazonaws.com
@@ -21,3 +23,7 @@ docker-compose -f docker-compose-grafana.yml up -d --pull always
 sleep 3
 
 docker-compose -f docker-compose-service.yml up -d --pull always
+
+sleep 3
+
+docker-compose -f docker-compose-model.yml up -d --pull always
